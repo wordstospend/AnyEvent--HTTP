2193c7e3817f63b50a3e6a4f1f05d7a4e8f0836c
root <root>
2011-01-18 09:33:35 +0000

    *** empty log message ***

--- HTTP.pm
+++ HTTP.pm
@@ -1334,7 +1334,7 @@ snippets.
 
 =head2 HTTP/1.1 FILE DOWNLOAD
 
-Downloading files with HTTP cna be quite tricky, especially when something
+Downloading files with HTTP can be quite tricky, especially when something
 goes wrong and you want tor esume.
 
 Here is a function that initiates and resumes a download. It uses the
